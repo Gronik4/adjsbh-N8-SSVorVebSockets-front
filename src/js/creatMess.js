d7573319ce9b871f5795fe40a33703e9d6931601
Gronik4 <golegni@yandex.ru>
2022-07-21 23:24:37 +0300

    Fixed the date error

--- src/js/creatMess.js
+++ src/js/creatMess.js
@@ -27,10 +27,10 @@ export default class CreatMess {
   static getTime() {
     const nd = new Date();
     const year = nd.getFullYear().toString().slice(2);
-    const manth = nd.getMonth() < 10 ? `0 + ${(nd.getMonth() + 1)}` : nd.getMonth() + 1;
-    const member = nd.getDate() < 10 ? `0 + ${nd.getDate()}` : nd.getDate();
-    const hours = nd.getHours() < 10 ? `0 + ${nd.getDate()}` : nd.getHours();
-    const minut = nd.getMinutes() < 10 ? `0 + ${nd.getDate()}` : nd.getMinutes();
+    const manth = nd.getMonth() < 10 ? `0${(nd.getMonth() + 1)}` : nd.getMonth() + 1;
+    const member = nd.getDate() < 10 ? `0${nd.getDate()}` : nd.getDate();
+    const hours = nd.getHours() < 10 ? `0${nd.getDate()}` : nd.getHours();
+    const minut = nd.getMinutes() < 10 ? `0${nd.getDate()}` : nd.getMinutes();
     const timeStamp = `${member}.${manth}.${year} ${hours}:${minut}`;
     return timeStamp;
   }
